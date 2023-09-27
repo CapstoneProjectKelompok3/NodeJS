8c0aeb8afbdf58373ceda700b7775a6f50682726
Fannn03 <fann.fann771@gmail.com>
2023-09-27 22:07:08 +0700

    feat(migrations): setup prisma

---
+++ prisma/migrations/20230927150258_create_table_users_documents/migration.sql
@@ -0,0 +1,35 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `level` ENUM('user', 'admin', 'superadmin') NOT NULL DEFAULT 'user',
+    `is_activated` BOOLEAN NOT NULL DEFAULT false,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `is_deleted` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `users_email_key`(`email`),
+    UNIQUE INDEX `users_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `documents` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `fullname` VARCHAR(255) NULL,
+    `nik` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(50) NULL,
+    `gender` ENUM('male', 'female') NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `is_deleted` BOOLEAN NOT NULL DEFAULT false,
+
+    UNIQUE INDEX `documents_user_id_key`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `documents` ADD CONSTRAINT `documents_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
