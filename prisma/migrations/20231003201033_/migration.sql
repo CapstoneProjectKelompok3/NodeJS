07f7c193cd736490f8556640329e59143f87fee6
Fannn03 <fann.fann771@gmail.com>
2023-10-05 20:39:08 +0700

    fix: migration in create table message and chatrooms

--- prisma/migrations/20231003201033_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `chatroom` MODIFY `name` VARCHAR(191) NULL,
-    MODIFY `adminId` INTEGER NULL;
--- prisma/migrations/20231005130718_create_table_message_and_room/migration.sql
+++ prisma/migrations/20231005130718_create_table_message_and_room/migration.sql
@@ -14,12 +14,13 @@ CREATE TABLE `Message` (
 -- CreateTable
 CREATE TABLE `ChatRoom` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `userId` INTEGER NOT NULL,
+    `adminId` INTEGER NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
     `is_deleted` BOOLEAN NOT NULL DEFAULT false,
 
-    UNIQUE INDEX `ChatRoom_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
