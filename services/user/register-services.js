6788fdfa4e998561499e130bcd3325fd8530c92e
Fannn03 <fann.fann771@gmail.com>
2023-10-05 11:50:47 +0700

    fix(register): duplicate nik

--- services/user/register-services.js
+++ services/user/register-services.js
@@ -1,32 +1,35 @@
 import 'dotenv/config'
 import { v4 as uuidv4 } from 'uuid'
 import jwt from 'jsonwebtoken'
-import { userRegister } from '../../repositories/user.js'
+import { userForceDelete, userRegister } from '../../repositories/user.js'
 import { documentRegister } from '../../repositories/document.js'
 import { sendRegisterMail } from '../../repositories/mail.js'
 import cache from '../../config/cache.js'
 
 export default async (request) => {
-    if(request.level === 'admin') request.is_activated = true
+	if(request.level === 'admin') request.is_activated = true
+	let user 
 
-    try {
-        let user = await userRegister(request)
-        await documentRegister(request, user.id)
+	try {
+		user = await userRegister(request)
+		await documentRegister(request, user.id)
 
-        // decoded user data
-        const payload=  {
-            id: user.id
-        }
-        const decoded = jwt.sign(payload, process.env.TOKEN_SECRET)
+		// decoded user data
+		const payload=  {
+			id: user.id
+		}
+		const decoded = jwt.sign(payload, process.env.TOKEN_SECRET)
 
-        // create a cache
-        const uuid = uuidv4()
-        cache.set(uuid, decoded, 30 * 60) // cache expired in 30 minutes
+		// create a cache
+		const uuid = uuidv4()
+		cache.set(uuid, decoded, 30 * 60) // cache expired in 30 minutes
 
-        sendRegisterMail(user.email, uuid)
-    } catch (err) {
-        return err
-    }
-
-    return true
+		sendRegisterMail(user.email, uuid)
+	} catch (err) {
+		if(err.code == "P2002" && err.meta.target == "documents_nik_key"){
+			console.log('document gagal', user);
+			await userForceDelete(user.id)
+		}
+		throw err
+	}
 }
