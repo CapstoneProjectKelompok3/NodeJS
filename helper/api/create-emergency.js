79758241eeb43d103e7d61446fc8d5d54e9d1ff5
Fannn03 <fann.fann771@gmail.com>
2023-10-07 11:53:21 +0700

    perf(emergencies): change to void function in create emergency

--- helper/api/create-emergency.js
+++ helper/api/create-emergency.js
@@ -1,10 +1,8 @@
 import axios from "./index.js"
 
 export default async (receiverId, req) => {
-  const request = await axios(req.token).post(`/users/${receiverId}/emergencies`, {
+  await axios(req.token).post(`/users/${receiverId}/emergencies`, {
     latitude: req.body.latitude,
     longitude: req.body.longitude
   })
-
-  return request.data.data
 }
