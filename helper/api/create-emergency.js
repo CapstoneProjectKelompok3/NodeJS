91f8f7d10949d8a9394384103876a23cf626f131
Fannn03 <fann.fann771@gmail.com>
2023-10-08 12:26:31 +0700

    feat: insert data emergency id on chat room

--- helper/api/create-emergency.js
+++ helper/api/create-emergency.js
@@ -1,7 +1,7 @@
 import axios from "./index.js"
 
 export default async (receiverId, req) => {
-  await axios(req.token).post(`/users/${receiverId}/emergencies`, {
+  return await axios(req.token).post(`/users/${receiverId}/emergencies`, {
     latitude: req.body.latitude,
     longitude: req.body.longitude
   })
